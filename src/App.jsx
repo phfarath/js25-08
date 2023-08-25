783303d2583aa1902b8b64fdc9e9a914323d4e9a
phfarath <pedropontesfarath@gmail.com>
2023-08-25 11:20:24 -0300

    js25-08

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Cabecalho from "./Cabecalho"
+import Footer from "./Footer"
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1>oi</h1>
+    <Cabecalho />
+    <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/Cabecalho.jsx
@@ -0,0 +1,10 @@
+import {}from 'react'
+
+function Cabecalho(){
+    return(
+        <>
+        <h1>Cabecalho teste </h1>
+        </>
+    )
+}
+export default Cabecalho 
